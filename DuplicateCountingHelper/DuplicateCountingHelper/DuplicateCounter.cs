ef67790e58a908fbb3a52b18096be8c2718f4c31
Hwan Wen Chen <munga0923@gmail.com>
2017-08-21 00:59:51 +0800

    refactor

--- DuplicateCountingHelper/DuplicateCountingHelper/DuplicateCounter.cs
+++ DuplicateCountingHelper/DuplicateCountingHelper/DuplicateCounter.cs
@@ -1,32 +1,17 @@
-﻿namespace DuplicateCountingHelper
+﻿using System.Linq;
+
+namespace DuplicateCountingHelper
 {
 	public class DuplicateCounter
 	{
 		public static int DuplicateCount(string str)
 		{
-			if (string.IsNullOrEmpty(str))
-			{
-				return 0;
-			}
 			var characters = str.ToCharArray();
-			var duplicateSymbolCounter = 0;
-			foreach (var symbolChar in characters)
-			{
-				var count = 0;
-				foreach (var targetChar in characters)
-				{
-					if (symbolChar == targetChar)
-					{
-						count++;
-					}
-				}
-				if (count > 1)
-				{
-					duplicateSymbolCounter++;
-				}
-			}
-
-			return duplicateSymbolCounter;
+			var duplicateSymbolCount =
+				characters.Select(symbolChar =>characters
+					.Count(targetChar => symbolChar == targetChar))
+					.Count(count => count > 1);
+			return duplicateSymbolCount;
 		}
 	}
 }
