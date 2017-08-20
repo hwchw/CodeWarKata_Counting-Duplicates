53863cbe9ff2b6cf227f7831570799cf8d88bc4d
Hwan Wen Chen <munga0923@gmail.com>
2017-08-20 17:46:44 +0800

    init kata

---
+++ DuplicateCountingHelper/DuplicateCountingHelper/DuplicateCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuplicateCountingHelper
+{
+    public class DuplicateCounter
+    {
+		 public static int DuplicateCount(string str)
+		 {
+			 return -1;
+		 }
+    }
+}
---
+++ DuplicateCountingHelper/DuplicateCountingHelperTests/DuplicateCounterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DuplicateCountingHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace DuplicateCountingHelper.Tests
+{
+	[TestClass()]
+	public class DuplicateCounterTests
+	{
+		[TestMethod()]
+		public void DuplicateCountTest()
+		{
+			Assert.AreEqual(0, DuplicateCounter.DuplicateCount(""));
+			//Assert.AreEqual(0, DuplicateCounter.DuplicateCount("abcde"));
+			//Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabbcde"));
+			//Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabBcde"), "should ignore case");
+			//Assert.AreEqual(1, DuplicateCounter.DuplicateCount("Indivisibility"));
+			//Assert.AreEqual(2, DuplicateCounter.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+		}
+	}
+}
