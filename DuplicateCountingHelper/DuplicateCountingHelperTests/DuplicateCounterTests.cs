38de6819c5d242592c5b0bda3e49108304eed758
Hwan Wen Chen <munga0923@gmail.com>
2017-08-21 01:04:46 +0800

    add third test case, get red light

--- DuplicateCountingHelper/DuplicateCountingHelperTests/DuplicateCounterTests.cs
+++ DuplicateCountingHelper/DuplicateCountingHelperTests/DuplicateCounterTests.cs
@@ -15,7 +15,7 @@ namespace DuplicateCountingHelper.Tests
 		{
 			Assert.AreEqual(0, DuplicateCounter.DuplicateCount(""));
 			Assert.AreEqual(0, DuplicateCounter.DuplicateCount("abcde"));
-			//Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabbcde"));
+			Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabbcde"));
 			//Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabBcde"), "should ignore case");
 			//Assert.AreEqual(1, DuplicateCounter.DuplicateCount("Indivisibility"));
 			//Assert.AreEqual(2, DuplicateCounter.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
